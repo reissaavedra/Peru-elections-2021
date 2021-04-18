827e1e794d3ea0cccce349e82aca8aed2a9038c0
Reisson Saavedra <reisson.saavedra.miguel@gmail.com>
2021-04-18 15:20:05 -0500

    parametrizing

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ consumerSecretKey = 'CONSUMER_SECRET_KEY'
 accessTokenKey = 'ACCESS_TOKEN_KEY'
 accessTokenSecretKey = 'ACCESS_TOKEN_SECRET_KEY'
 
+#Parametros de ejecucion
+retryDelay = 180
+
+
 
 def loadParametersYml():
     with open(PATH_YAML_CONFIG) as file:
@@ -22,7 +26,7 @@ def configTweepy():
     api = tweepy.API(auth,
                      # parser=tweepy.parsers.JSONParser(),
                      wait_on_rate_limit=True,
-                     retry_delay=60 * 3,
+                     retry_delay=retryDelay,
                      retry_count=5,
                      retry_errors=set([401, 404, 500, 503]),
                      wait_on_rate_limit_notify=True)
@@ -30,26 +34,10 @@ def configTweepy():
 
 
 def extractTweets():
-    # allTweets = []
-    # screen_name = '@userToSearch'
-    # new_tweets = api.user_timeline(screen_name=screen_name, count=500)
-    # allTweets.extend(new_tweets)
-    # oldest = allTweets[-1].id - 1
-    #
-    # while len(new_tweets) > 0:
-    #     print(f"getting tweets before {oldest}")
-    #     new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
-    #     allTweets.extend(new_tweets)
-    #     oldest = allTweets[-1].id - 1
-    #     print(f"...{len(allTweets)} tweets downloaded so far")
-    #
-    # outTweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in allTweets]
-    # return outTweets
-
     if configTweepy():
         cursorTweepy = tweepy.Cursor(configTweepy().search,
                                      q=["Pedro Castillo", 'pedro castillo', 'Peru Libre'],
-                                     lang='en',
+                                     lang=['en','es'],
                                      since="2021-04-11").items(100000000)
         # return
         uploadTweets2Csv(cursorTweepy)
