e4180891e986b53377c2ec99cd7a31bd29851ecc
Reisson Saavedra <reisson.saavedra.miguel@gmail.com>
2021-04-18 12:53:22 -0500

    toCsv data

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from yaml import load, FullLoader
 import tweepy
+import os
+
 PATH_YAML_CONFIG = r'./config/config.yml'
 consumerKey='CONSUMER_KEY'
 consumerSecretKey= 'CONSUMER_SECRET_KEY'
@@ -12,35 +14,81 @@ def loadParametersYml():
         return parameters
 
 def configTweepy():
-    parametersYml = loadParametersYml()
-    auth = tweepy.OAuthHandler(parametersYml[consumerKey], parametersYml[consumerSecretKey])
-    auth.set_access_token(parametersYml[accessTokenKey], parametersYml[accessTokenSecretKey])
+    paramYml = loadParametersYml()
+    auth = tweepy.OAuthHandler(paramYml[consumerKey], paramYml[consumerSecretKey])
+    auth.set_access_token(paramYml[accessTokenKey], paramYml[accessTokenSecretKey])
     api = tweepy.API(auth,
+                     parser=tweepy.parsers.JSONParser(),
                      wait_on_rate_limit=True,
                      retry_delay=60 * 3,
                      retry_count=5,
                      retry_errors=set([401, 404, 500, 503]),
                      wait_on_rate_limit_notify=True)
-    return api
+    return api if api.verify_credentials() else False
 
 def extractTweets():
-    allTweets = []
+    # allTweets = []
+    # screen_name = '@userToSearch'
+    # new_tweets = api.user_timeline(screen_name=screen_name, count=500)
+    # allTweets.extend(new_tweets)
+    # oldest = allTweets[-1].id - 1
+    #
+    # while len(new_tweets) > 0:
+    #     print(f"getting tweets before {oldest}")
+    #     new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
+    #     allTweets.extend(new_tweets)
+    #     oldest = allTweets[-1].id - 1
+    #     print(f"...{len(allTweets)} tweets downloaded so far")
+    #
+    # outTweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in allTweets]
+    # return outTweets
+    new_search = ["keiko"]
+    date_since = "2021-04-15"
     api = configTweepy()
-    screen_name = '@userToSearch'
-    new_tweets = api.user_timeline(screen_name=screen_name, count=500)
-    allTweets.extend(new_tweets)
-    oldest = allTweets[-1].id - 1
+    if api:
+        tweets = api.search(q=new_search,
+                            lang='es',
+                            since=date_since)
+        # return [[
+        #     # tweet.author,
+        #     tweet.contributors,
+        #     tweet.coordinates,
+        #     tweet.created_at,
+        #     tweet.favorited,
+        #     tweet.geo,
+        #     tweet.id_str,
+        #     tweet.in_reply_to_screen_name,
+        #     tweet.in_reply_to_user_id,
+        #     tweet.is_quote_status,
+        #     tweet.lang,
+        #     tweet.place,
+        #     tweet.retweet_count,
+        #     tweet.retweeted,
+        #     tweet.retweets,
+        #     tweet.source,
+        #     tweet.source_url,
+        #     tweet.text,
+        #     tweet.truncated,
+        #     tweet.user.listed_count,
+        #     tweet.user.location,
+        #     tweet.user.name,
+        #     tweet.user.screen_name,
+        #     tweet.user.statuses_count,
+        #     tweet.user.time_zone,
+        #     tweet.user.verified,
+        #     tweet.user.follow_request_sent,
+        #     tweet.user.friends_count,
+        #     tweet.user.following,
+        #     tweet.user.created_at] for tweet in tweets]
+        uploadTweets2Csv(tweets)
+        # return tweets
 
-    while len(new_tweets) > 0:
-        print(f"getting tweets before {oldest}")
-        new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=oldest)
-        allTweets.extend(new_tweets)
-        oldest = allTweets[-1].id - 1
-        print(f"...{len(allTweets)} tweets downloaded so far")
 
-    outTweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in allTweets]
-    return outTweets
-
+def uploadTweets2Csv(tweets):
+    path = './dataCsv'
+    os.makedirs(path, exist_ok=True)
+    print(tweets)
 
 if __name__ == '__main__':
-    print(extractTweets())
+    # print(len(extractTweets()['statuses']))
+    extractTweets()
