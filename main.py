75c920e0d121a744e442d3bad52076bfea04dc5a
reissonsaavedramiguel <reisson.saavedra.miguel@gmail.com>
2021-04-21 09:28:40 -0500

    fix problem from visual code

--- main.py
+++ main.py
@@ -33,23 +33,25 @@ def extractTweets():
 
 def uploadTweets2Csv(tweets, candidate, lang):
     fileName = os.path.join(PATH_DATA_CSV, f'{candidate}_{lang}.csv')
+    print(f"Candidate: {candidate}, Language: {lang} \n")
     import time
-    with open(fileName, 'a') as file:
-        try:
-            writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            writer.writerow(TweetBatch.TweetBatch.columns)
-            i = 0
-            for tweet in tweets:
+    with open(fileName, 'a', encoding="utf-8") as file:
+        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(TweetBatch.TweetBatch.columns)
+        i = 0
+        for tweet in tweets:
+            try:
                 tweetBatch = TweetBatch.TweetBatch(tweet._json)
                 writer.writerow(tweetBatch.__dict__.values())
                 i += 1
                 if i % 100 == 0:
-                    time.sleep(60 * random.randint(4, 10))
-        except Exception as e:
-            print(e)
-        finally:
-            print(f"Candidate: {candidate}, Language: {lang}, Tweets extraidos : {i}  \n")
-
+                    sleepTime = random.randint(2,5)
+                    print(f'Stoping {sleepTime} seconds....')
+                    time.sleep(60 * sleepTime)
+                    print('Reanunding....')
+            except Exception as e:
+                print(e)
+                continue
 
 if __name__ == '__main__':
     extractTweets()
