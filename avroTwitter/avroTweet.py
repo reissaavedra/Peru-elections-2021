709ce4d9d5d69e5b98b3dea67f6722f13d86af87
Reisson Saavedra <reisson.saavedra.miguel@gmail.com>
2021-04-27 17:49:58 -0500

    Tweet avro schema

--- avroTwitter/avroTweet.py
+++ avroTwitter/avroTweet.py
@@ -5,13 +5,15 @@ import os
 
 path_schema = './../schema'
 path_avro_file = './../avro'
+tweet_input = 'tweet_input.json'
+tweet_output_avro = 'tweet_output.avro'
 
 schema = avro.schema.parse(
     open(os.path.join(path_schema,'schemaTwitter.avsc'), 'rb').read()
 )
 
 writer = DataFileWriter(
-    open(os.path.join(path_avro_file, 'tweet.avro'), 'wb'), DatumWriter(), schema
+    open(os.path.join(path_avro_file, 'tweet_output.avro'), 'wb'), DatumWriter(), schema
 )
 
 writer.append({'name': 'Alyssa','favorite_number': 256})
